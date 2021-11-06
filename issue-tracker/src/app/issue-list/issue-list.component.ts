8e9108838bba53ec030129c7c9b84af64cf38905
Aokami-dvlp <emanuele.david3@gmail.com>
2021-11-06 12:01:34 +0100

    Added many features
    - Add new issues
    - Validating new issues
    - Resolve Issues

--- issue-tracker/src/app/issue-list/issue-list.component.ts
+++ issue-tracker/src/app/issue-list/issue-list.component.ts
@@ -11,6 +11,8 @@ import { IssuesService } from '../issues.service';
 export class IssueListComponent implements OnInit {
 
   issues: Issue[] = [];
+  showReportIssue = false;
+  selectedIssue: Issue | null = null;
 
   constructor(private issueService: IssuesService) { }
 
@@ -20,6 +22,19 @@ export class IssueListComponent implements OnInit {
 
   private getIssues(){
     this.issues = this.issueService.getPendingIssues();
+  }
+
+  onCloseReport(){
+    this.showReportIssue = false;
+    this.getIssues();
+  }
+
+  onConfirm(confirmed: boolean) {
+    if(confirmed && this.selectedIssue){
+      this.issueService.completeIssue(this.selectedIssue);
+      this.getIssues();
+    }
+    this.selectedIssue = null;
   }
 
 }
